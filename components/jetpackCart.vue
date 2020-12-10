e8421965091b72efd7a7aed5defcdacece3c1835
Ryan Pamplin <ryan@blendjet.com>
2020-12-09 16:40:45 -0800

    JetPack 6 cart upsell mod

--- components/jetpackCart.vue
+++ components/jetpackCart.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="jetpack-container" v-if="product && jetpacks">
     <div class="text-block">
-                               <a href="/products/banana-blueberry-jetpack-ready-to-blend-smoothie" style="color:white;">Try JetPack Variety Pack</a><br/> <span style="font-size: 75%;color:gold;">5 Ready-to-Blend Smoothies</span>
+                               <a href="/products/banana-blueberry-jetpack-ready-to-blend-smoothie" style="color:white;">Try JetPack Variety Pack</a><br/> <span style="font-size: 75%;color:gold;">6 Ready-to-Blend Smoothies</span>
     </div>
     <div class="jetpack-image">
       <transition name="fade">
@@ -89,7 +89,7 @@ export default {
       subUnitDisplayPrice: 2.99,
       unitDisplayPrice: 3.99,
       subscriptionChecked: true,
-      displayPrice: 35.88,
+      displayPrice: 17.94,
       localizedUnitPrice: '2.99',
       symbol: '$'
     }
@@ -142,11 +142,11 @@ export default {
       if(check) {
         this.toggleSubscriptions(true)
         this.subscriptionChecked = true
-        this.displayPrice = (this.subUnitDisplayPrice * 5) 
+        this.displayPrice = (this.subUnitDisplayPrice * 6) 
       } else {
         this.toggleSubscriptions(false)
         this.subscriptionChecked = false
-        this.displayPrice = (this.unitDisplayPrice * 5) 
+        this.displayPrice = (this.unitDisplayPrice * 6) 
       }
     },
     toggleSubscriptions(sub) {
@@ -241,7 +241,7 @@ export default {
               		if(this.symbol == null){this.symbol = res.data.Currency}
               this.subUnitDisplayPrice = res.data.ConsumerPrices[0]
               this.unitDisplayPrice = res.data.ConsumerPrices[1]
-              this.subscriptionChecked ? this.displayPrice = this.subUnitDisplayPrice * 5 : this.displayPrice = this.unitDisplayPrice * 5
+              this.subscriptionChecked ? this.displayPrice = this.subUnitDisplayPrice * 6 : this.displayPrice = this.unitDisplayPrice * 6
             }
           })
           .catch((res) => {
