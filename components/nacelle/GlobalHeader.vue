876e075c9f80aea7ca68ffabb39af2a7b1f12f5d
Ryan Pamplin <ryan@blendjet.com>
2020-12-16 19:59:08 -0800

    Arrives by Christmas Header

--- components/nacelle/GlobalHeader.vue
+++ components/nacelle/GlobalHeader.vue
@@ -211,7 +211,7 @@ export default {
       screenWidth: 0,
       logoFill: '#373975',
       jetpackMenuVisible: false,
-      freeShippingMessage: 'HOLIDAY SALE - SAVE UP TO 20%',
+      freeShippingMessage: 'ARRIVES BEFORE CHRISTMAS',
       navColor: 'white',
       scrollY: 0,
       scrollTarget: 200,
