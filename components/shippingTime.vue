a17bb5278a498a83029b06f8819d40638ef16635
ryanpamplin <ryan@blendjet.com>
2020-12-20 23:32:21 -0800

    Update shippingTime.vue

--- components/shippingTime.vue
+++ components/shippingTime.vue
@@ -30,7 +30,7 @@
 
 <script>
 
-var transitTime  = 5; // US ARRIVAL DATE
+var transitTime  = 4; // US ARRIVAL DATE
 var cutoffHour = 15; //3PM cutoff time
 var timezoneOffset = (new Date().getTimezoneOffset()/60)-8; //users offset from PST
 	
