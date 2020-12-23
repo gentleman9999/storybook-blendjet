29fc99a94c975fa20733cfb9ce4431150b3417a7
ryanpamplin <ryan@blendjet.com>
2020-12-23 12:51:38 -0800

    Update shippingTime.vue

--- components/shippingTime.vue
+++ components/shippingTime.vue
@@ -4,16 +4,16 @@
 
   <div class="shipping-container" v-if="!disabled">
     <div v-if="size ==='short'" class="small-size">
-      <span v-if="country == 'US'">Arrives {{shortDate}}</span> <!-- change X to 2 to apply fixed date -->
+      <span v-if="country == 'xxUS'">Arrives {{shortDate}}</span> <!-- change X to 2 to apply fixed date -->
       <span v-else>Ships by {{shortDate}}</span>
     </div>
     <div v-else class="normal-size">
       <div class="normal-size__label">
-        <span v-if="country == 'US'">Want it by {{arrivalDate}}?</span>
+        <span v-if="country == 'xxUS'">Want it by {{arrivalDate}}?</span>
         <span v-else>Order now and it ships by</span>
       </div>
       <div class="normal-size__countdown" v-if="size !=='short'">
-	  	<span v-if="country == 'US'">
+	  	<span v-if="country == 'xxUS'">
         <span v-if="24-remaining.hours === 0">Order within {{60-remaining.minutes}} minutes</span>
         <span v-else-if="24-remaining.hours === 1">Order within {{24-remaining.hours}} hour {{60-remaining.minutes}} minutes</span>
         <span v-else >Order within {{24-remaining.hours}} hours {{60-remaining.minutes}} minutes</span>
@@ -30,7 +30,7 @@
 
 <script>
 
-var transitTime  = 4; // US ARRIVAL DATE
+var transitTime  = 5; // US ARRIVAL DATE
 var cutoffHour = 15; //3PM cutoff time
 var timezoneOffset = (new Date().getTimezoneOffset()/60)-8; //users offset from PST
 	
