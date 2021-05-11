bf8a74a82a2924198c85c993b8ccf3c7c035aa37
Zachary Schaffter <zachary.schaffter@gmail.com>
2021-05-10 17:36:55 -0700

    homepage - remove sale banner

--- pages/index.vue
+++ pages/index.vue
@@ -36,7 +36,7 @@
       </div>
     </div>
     <!-- START BFCM -->
-    <div class="outer-canvas-bfcm" style="margin-bottom: unset;">
+    <!-- <div class="outer-canvas-bfcm" style="margin-bottom: unset;">
       <div class="canvas-bfcm">
         <div class="col-bfcm">
           <span class="entry-title-bfcm">Spring Sale</span>
@@ -44,7 +44,7 @@
           <span class="simple-text-bfcm">With Each BlendJet <b class="b-hide-bfcm">|</b> <span>Discount Applied at Checkout</span></span>
         </div>
       </div>
-    </div>
+    </div> -->
     <!-- END BFCM -->
       <div class="section section__as-seen-on">
         <Ticker type="asSeenOn"/>
