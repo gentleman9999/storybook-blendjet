25231b5d01a6ffc93c3fa9e5de1038ca8eac3f55
gentleman9999 <gentleman99729@outlook.com>
2021-11-09 21:53:02 +0530

    modify tolerance

--- plugins/vueTouchEvents.js
+++ plugins/vueTouchEvents.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
 import Vue2TouchEvents from 'vue2-touch-events'
 
-Vue.use(Vue2TouchEvents)
+Vue.use(Vue2TouchEvents, {
+    disableClick: true,
+    touchClass: '',
+    tapTolerance: 5,
+    touchHoldTolerance: 400,
+    swipeTolerance: 40,
+    longTapTimeInterval: 400,
+    namespace: 'touch'
+})
