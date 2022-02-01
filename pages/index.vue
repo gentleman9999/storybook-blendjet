c0eb78c57a2116f29ec45791da2d0ded6a001ba1
ryanpamplin <ryan@blendjet.com>
2022-02-01 18:28:42 -0400

    Update to Valentine's Sale

--- pages/index.vue
+++ pages/index.vue
@@ -58,7 +58,7 @@
 
       <!-- START BFCM -->
 
-<div class="outer-canvas-bfcm" style="margin-bottom:unset" data-v-1b05d426=""><div class="canvas-bfcm" data-v-1b05d426=""><div class="col-bfcm" data-v-1b05d426=""><span class="entry-title-bfcm" data-v-1b05d426="">Valentine's Sale</span> <span class="content-split-element-bfcm" data-v-1b05d426="">3 Free JetPack Smoothies</span> <span class="simple-text-bfcm" data-v-1b05d426="">With Each BlendJet <b class="b-hide-bfcm" data-v-1b05d426="">|</b> <span data-v-1b05d426="">or 6 FREE WHEN YOU SUBSCRIBE</span></span></div></div></div>
+<div class="outer-canvas-bfcm" style="margin-bottom:unset" data-v-1b05d426=""><div class="canvas-bfcm" data-v-1b05d426=""><div class="col-bfcm" data-v-1b05d426=""><span class="entry-title-bfcm" data-v-1b05d426="">Valentine's Sale</span> <span class="content-split-element-bfcm" data-v-1b05d426="">Choose 3 Free JetPacks</span> <span class="simple-text-bfcm" data-v-1b05d426="">With Each BlendJet <b class="b-hide-bfcm" data-v-1b05d426="">|</b> <span data-v-1b05d426="">or 6 FREE WHEN YOU SUBSCRIBE</span></span></div></div></div>
 
       <!-- END BFCM -->
 
