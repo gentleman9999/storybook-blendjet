1e3f0ddfdd6b49ee4a74e61f098d77335b6d1022
gentleman9999 <gentleman99729@outlook.com>
2022-02-14 18:22:47 +0530

    Custom Marquee

---
+++ components/CustomMarquee.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="marquee-wrapper">
+    <div class="marquee-container">
+      <div
+        class="marquee-block"
+        :style="[{ width: width * length + 'px' }, { height: height ? height + 'px' : '60px' }]"
+      >
+        <div class="marquee-inner" :class="direction">
+          <span>
+            <slot></slot>
+          </span>
+          <span>
+            <slot></slot>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 100
+    },
+    height: {
+      type: Number,
+      default: 60
+    },
+    length: {
+      type: Number,
+      default: 0
+    },
+    direction: {
+      type: String,
+      default: 'left'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.marquee-wrapper {
+  text-align: center;
+}
+.marquee-wrapper .marquee-container {
+  overflow: hidden;
+}
+.marquee-inner span {
+  float: left;
+  width: 50%;
+}
+.marquee-wrapper .marquee-block {
+  height: 60px;
+  overflow: hidden;
+  box-sizing: border-box;
+  position: relative;
+}
+.marquee-inner {
+  display: block;
+  width: 200%;
+  position: absolute;
+}
+.marquee-inner p {
+  font-weight: 800;
+  font-size: 30px;
+  font-family: cursive;
+}
+.marquee-inner.left {
+  animation: marqueeLeft 50s linear infinite;
+}
+.marquee-inner.right {
+  animation: marqueeRight 50s linear infinite;
+}
+.marquee-item {
+  height: 60px;
+  min-width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 10px;
+  color: white;
+  float: left;
+  transition: all 0.2s ease-out;
+}
+@keyframes marqueeLeft {
+  0% {
+    left: 0;
+  }
+  100% {
+    left: -100%;
+  }
+}
+@keyframes marqueeRight {
+  0% {
+    left: -100%;
+  }
+  100% {
+    left: 0;
+  }
+}
+</style>
